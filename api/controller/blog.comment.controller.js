0afb6a5ec2e9c2780328e47b9384832dd86cdaba
poseidon2022 <kidusm82@gmail.com>
2024-09-01 20:52:15 +0300

    comment controller start

--- api/controller/blog.comment.controller.js
+++ api/controller/blog.comment.controller.js
@@ -4,7 +4,13 @@ class BlogCommentController {
     }
 
     async CreateComment(req, res) {
+        const blog_id = req.params["blog_id"]
+        try {
+            
+        }
+        catch(err) {
 
+        }
     }
 
     async UpdateComment(req, res) {
