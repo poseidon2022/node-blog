bfe32db72021651a6e3049ac6200a4b95d36cdb0
poseidon2022 <kidusm82@gmail.com>
2024-09-01 21:24:32 +0300

    create comment controller

--- api/controller/blog.comment.controller.js
+++ api/controller/blog.comment.controller.js
@@ -1,3 +1,5 @@
+const mongoose = require("mongoose")
+const Comment = require("../../models/blog.comment.model")
 class BlogCommentController {
     constructor(blogCommentUseCase) {
         this.blogCommentUseCase = blogCommentUseCase
@@ -5,11 +7,23 @@ class BlogCommentController {
 
     async CreateComment(req, res) {
         const blog_id = req.params["blog_id"]
+        // TODO : get the user ID from req.user.userid man.
+        //for now with a demo user ID 
+        const user_id = mongoose.Types.ObjectId()
+        const {content} = req.body
         try {
-            
+            const newComment = new Comment({
+                user_id : user_id,
+                blog_id : blog_id,
+                content : content
+            })
+
+            const createdComment = await blogCommentUseCase.createdComment(createdComment)
+            res.json({message : createdComment})
         }
         catch(err) {
-
+            console.error(err.message)
+            res.status(500).json({error : err.message})
         }
     }
 
