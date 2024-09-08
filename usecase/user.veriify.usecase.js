d23eb11e36414fcdd583ff291861f0dfd1465366
poseidon2022 <kidusm82@gmail.com>
2024-09-08 09:25:41 +0300

    signup usecase started

---
+++ usecase/user.veriify.usecase.js
@@ -0,0 +1,9 @@
+class SignupUseCase {
+    constructor(signupRepository) {
+        this.signupRepository = signupRepository
+    }
+    
+    async Signup(name, email, password, otp) {
+        
+    }
+}
