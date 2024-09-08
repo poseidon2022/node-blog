6220d1ea1ecc9e77a578a07619e85b8899607dea
poseidon2022 <kidusm82@gmail.com>
2024-09-08 09:17:18 +0300

    signup controller done

---
+++ api/controller/user.verify.controller.js
@@ -0,0 +1,32 @@
+const bcrypt = require("bcrypt")
+class SignupController {
+    constructor(signupUseCase) {
+        this.signupUseCase = signupUseCase
+    }
+
+    async Signup(req, res) {
+        const {name, email, password, otp} = req.body
+        if (!name || !email || !password || !otp) {
+            res.status(403).json({
+                success : false,
+                message : "invalid request format, required information missing"
+            })
+            const hashedPwd = await bcrypt.hash(password, 10)
+            const newUser = await this.signupUseCase.Signup(name,email,hashedPwd, otp)
+            res.status(200).json({
+                success : true,
+                message : "user created successfully",
+                user : newUser
+            })
+        }
+        try {
+        } catch(err) {
+            res.status(500).json({
+                success : false,
+                message : err.message
+            })
+        }
+    }
+}
+
+module.exports = SignupController
