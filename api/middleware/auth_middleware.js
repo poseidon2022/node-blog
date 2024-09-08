a80c991f700d281965b05b5b60497ee021f294fc
poseidon2022 <kidusm82@gmail.com>
2024-09-08 21:03:01 +0300

    auth middleware done

--- api/middleware/auth_middleware.js
+++ api/middleware/auth_middleware.js
@@ -1,4 +1,24 @@
+require("dotenv").config()
+const jwt = require("jsonwebtoken")
 const authenticate = (req, res, next) => {
     const token = req.headers['authorization']
-    
+    if (!token) {
+        return res.status(401).json({
+            success : false,
+            message : "access token not provided"
+        })
+    }
+
+    try {
+        const decoded = jwt.verify(token, process.env.JWT_SECRET)
+        req.user = decoded
+        next()
+    } catch(err) {
+        res.status(400).json({
+            success : false,
+            message : "invalid access token"
+        })
+    }
 }
+
+module.exports = authenticate
