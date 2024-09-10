c903d54e4d42248a9a123626b247fadc08e84ad3
poseidon2022 <kidusm82@gmail.com>
2024-09-10 15:47:46 +0300

    middle_ware modified

--- api/middleware/auth_middleware.js
+++ api/middleware/auth_middleware.js
@@ -1,8 +1,11 @@
 require("dotenv").config()
+const generateAccessToken = require("../utils/createaccesstoken")
+const generateRefreshToken = require("../utils/createrefreshtoken")
 const jwt = require("jsonwebtoken")
 const authenticate = (req, res, next) => {
-    const token = req.headers['authorization']
-    if (!token) {
+    const accessToken = req.headers['authorization']
+    const refreshToken = req.cookies["refreshToken"]
+    if (!accessToken && !refreshToken) {
         return res.status(401).json({
             success : false,
             message : "access token not provided"
@@ -10,14 +13,36 @@ const authenticate = (req, res, next) => {
     }
 
     try {
-        const decoded = jwt.verify(token, process.env.JWT_SECRET)
+        const decoded = jwt.verify(accessToken, process.env.JWT_SECRET)
         req.user = decoded
         next()
     } catch(err) {
-        res.status(400).json({
-            success : false,
-            message : "invalid access token"
-        })
+        if (!refreshToken) {
+            return res.status(401).json({
+                success : false,
+                message : "access denied. no refresh token provided"
+            })
+        } 
+
+        try {
+            const decodedUser = jwt.verify(refreshToken, process.env.JWT_SECRET)
+            const accessToken = generateAccessToken(decodedUser)
+            const refreshToken = generateRefreshToken(decodedUser)
+
+            return res
+                .header("Authorization", accessToken)
+                .cookies("refreshToken", refreshToken, {httpOnly : true, sameSite : 'strict'})
+                .json({
+                    success : true,
+                    message : "user authorized successsfully"
+                })
+            next()
+        } catch(err) {
+            res.status(400).json({
+                success : false,
+                message : "invalid token"
+            })
+        }
     }
 }
 
