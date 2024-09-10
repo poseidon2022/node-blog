e3a99eb84d3ea91784d6bd0b8e1aef4b49a56024
poseidon2022 <kidusm82@gmail.com>
2024-09-10 17:04:07 +0300

    refresh access token repo modified

--- repository/user.refreshToken.repository.js
+++ repository/user.refreshToken.repository.js
@@ -2,7 +2,7 @@ const Refresh = require("../models/refresh.model")
 class RefreshTokenRepository {
     async RefreshAccessToken(email) {
         try {
-            const refreshTokenInfo = await Refresh.find({email})
+            const refreshTokenInfo = await Refresh.find({email}).sort({created_at : -1}).limit(1)
             return refreshTokenInfo
         } catch(err) {
             throw new Error("error while fetching refresh token info")
